4fa6111eeb2b0dc21352b9f0ae844a8ddfcb159d
Quentin Gliech <quentingliech@gmail.com>
2021-05-17 17:46:05 +0200

    Improve docker image

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-## Licensed under the terms of http://www.apache.org/licenses/LICENSE-2.0
 
-exec "$JAVA_HOME/bin/java"               \
-  $JAVA_OPTIONS                          \
-  -javaagent:"${FUSEKI_DIR}/${OTEL_JAR}" \
-  -Xshare:off                            \
-  -jar "${FUSEKI_DIR}/${FUSEKI_JAR}"     \
+export OTEL_RESOURCE_ATTRIBUTES="container.id=$(hostname),$OTEL_RESOURCE_ATTRIBUTES"
+
+exec "$JAVA_HOME/bin/java"                \
+  $JAVA_OPTIONS                           \
+  -javaagent:"${FUSEKI_HOME}/${OTEL_JAR}" \
+  -Xshare:off                             \
+  -Dlog4j.configurationFile="file:${FUSEKI_HOME}/log4j2.properties" \
+  -jar "${FUSEKI_HOME}/${FUSEKI_JAR}"     \
   "$@"
