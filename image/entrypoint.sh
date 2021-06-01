c8dea8518a3809485aa972c1d350073ded406988
Ludovic Muller <ludovicmuller1@gmail.com>
2021-06-01 16:51:58 +0200

    build Docker image with GeoSPARQL support

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -2,10 +2,17 @@
 
 export OTEL_RESOURCE_ATTRIBUTES="container.id=$(hostname),$OTEL_RESOURCE_ATTRIBUTES"
 
-exec "$JAVA_HOME/bin/java"                \
-  $JAVA_OPTIONS                           \
+exec "$JAVA_HOME/bin/java" \
+  $JAVA_OPTIONS \
   -javaagent:"${FUSEKI_HOME}/${OTEL_JAR}" \
-  -Xshare:off                             \
+  -Xshare:off \
   -Dlog4j.configurationFile="file:${FUSEKI_HOME}/log4j2.properties" \
-  -jar "${FUSEKI_HOME}/${FUSEKI_JAR}"     \
+  -jar "${FUSEKI_HOME}/${GEOSPARQL_JAR}" \
+  --loopback false \
+  -t "${FUSEKI_BASE}/databases/ds" \
+  -t2 \
+  --default_geometry \
+  --validate \
   "$@"
+
+  # --inference \
