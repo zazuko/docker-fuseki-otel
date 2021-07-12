f53da75b9510b08acd6e5a957e445a635f8c87d0
Quentin Gliech <quentingliech@gmail.com>
2021-07-12 17:50:44 +0200

    Support setting the admin password via shiro.ini

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -2,6 +2,12 @@
 
 export OTEL_RESOURCE_ATTRIBUTES="container.id=$(hostname),$OTEL_RESOURCE_ATTRIBUTES"
 
+if ! [ -z "$ADMIN_PASSWORD" ]; then
+  envsubst '$ADMIN_PASSWORD' \
+    < "${FUSEKI_HOME}/shiro.ini.tmpl" \
+    > "${FUSEKI_BASE}/shiro.ini"
+fi
+
 exec "$JAVA_HOME/bin/java"                \
   $JAVA_OPTIONS                           \
   -javaagent:"${FUSEKI_HOME}/${OTEL_JAR}" \
