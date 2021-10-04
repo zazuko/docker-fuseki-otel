64ee454a36b68c6261e0d61ff4684fa9377b2208
Quentin Gliech <quentingliech@gmail.com>
2021-10-04 08:31:09 +0200

    Wildfly-base image

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+set -e
+
 export OTEL_RESOURCE_ATTRIBUTES="container.id=$(hostname),$OTEL_RESOURCE_ATTRIBUTES"
+export JAVA_OPTS="${JAVA_OPTS} -javaagent:${OTEL_HOME}/${OTEL_JAR} -Xshare:off"
 
 if ! [ -z "$ADMIN_PASSWORD" ]; then
   envsubst '$ADMIN_PASSWORD' \
@@ -8,10 +11,4 @@ if ! [ -z "$ADMIN_PASSWORD" ]; then
     > "${FUSEKI_BASE}/shiro.ini"
 fi
 
-exec "$JAVA_HOME/bin/java"                \
-  $JAVA_OPTIONS                           \
-  -javaagent:"${FUSEKI_HOME}/${OTEL_JAR}" \
-  -Xshare:off                             \
-  -Dlog4j.configurationFile="file:${FUSEKI_HOME}/log4j2.properties" \
-  -jar "${FUSEKI_HOME}/${FUSEKI_JAR}"     \
-  "$@"
+exec $@
